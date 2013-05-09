9b9ec1896d2718d90abf585f02c6373189fc51c9
auciferjay <auciferjay@gmail.com>
2013-05-09 16:58:39 +0800

    update timerbase

--- src/cn/royan/hl/bases/TimerBase.hx
+++ src/cn/royan/hl/bases/TimerBase.hx
@@ -35,8 +35,6 @@ class TimerBase implements IDisposeBase
 		delay = time;
 		callFun = f;
 		loopLimit = loop;
-		
-		timerlists.push(this);
 	}
 	
 	public function start():Void
@@ -51,17 +49,19 @@ class TimerBase implements IDisposeBase
 			#end
 		}
 		
-		//if( !isStart ) timerNumber++;
 		isStart = true;
 		last = Std.int( Timer.stamp() * 1000 );
 		begin = last;
 		jumped = 0;
+		
+		timerlists.push(this);
 	}
 	
 	public function stop():Void
 	{
-		//if( isStart ) timerNumber--;
 		isStart = false;
+		
+		timerlists.remove(this);
 	}
 
 	public function remain():Int
@@ -94,10 +94,6 @@ class TimerBase implements IDisposeBase
 	public function dispose():Void
 	{
 		timerlists.remove(this);
-		//if ( isStart ) {
-		//	isStart = false;
-		//	timerNumber--;
-		//}
 		#if ( flash || js ) 
 		if ( timerNumber <= 0 )
 			timer.stop(); 
@@ -108,7 +104,6 @@ class TimerBase implements IDisposeBase
 	
 	private static function timerHandler(#if(cpp || neko) evt:Event #end):Void
 	{
-		//if ( timerNumber <= 0 ) return;
 		for ( time in timerlists ) {
 			var len:Int = time.needRender();
 			for ( i in 0...len ) {
